9663842a3f3bee8c082c3e93ec3c77fb8b2983e9
Mildred Naab <mwinkomnaab@gmail.com>
2025-04-15 19:23:28 +0000

    Add character,word and sentence count functionality

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ const body = document.body;
 const toggleBtn = document.getElementById("mode-button");
 const toggleIcon = document.querySelector(".light_dark_icon");
 
+
+//LIGHT/DARK MODE TOGGLE
+
 toggleBtn.addEventListener("click", () => {
   const isDark = body.classList.contains("dark-mode");
   const logo = document.querySelector(".logo");
   const upDownIcon = document.querySelector("path");
+  // const seeLess = document.querySelector(".see-button");
 
   if (isDark) {
     // Change to light mode
@@ -14,6 +18,7 @@ toggleBtn.addEventListener("click", () => {
     toggleIcon.src = "assets/images/icon-moon.svg";
     logo.src = "assets/images/logo-light-theme.svg"
     upDownIcon.fill = "#12131A"; 
+    // seeLess.innerHTML = "See less";
   } else {
     // Change to dark mode
     body.classList.remove("light-mode");
@@ -21,5 +26,95 @@ toggleBtn.addEventListener("click", () => {
     toggleIcon.src = "assets/images/icon-sun.svg";
     logo.src = "assets/images/logo-dark-theme.svg";
     upDownIcon.fill = "#E4E4EF"; 
+    
   }
 });
+
+//CHARACTER COUNTER
+const textarea = document.getElementById("textarea");
+const totalCharacters = document.getElementById("totalCharacters");
+const wordCount = document.getElementById("wordCount");
+const sentenceCount = document.getElementById("sentenceCount");
+const excludeSpaces = document.getElementById("exclude-spaces");
+
+
+textarea.addEventListener("input", () => {
+  const text = textarea.value;
+
+  //Character Count
+  const characters = text.length;
+  if (excludeSpaces.checked) {
+    const textWithoutSpaces = text.replace(/\s/g, "");
+    totalCharacters.textContent = String(textWithoutSpaces.length).padStart(2, "0");
+  }
+  else if (excludeSpaces.unchecked){
+    totalCharacters.textContent = String(characters).padStart(2, "0");
+  }
+  else {
+    totalCharacters.textContent = String(characters).padStart(2, "0");
+  }
+
+  //Word Count
+  let wordNumber = 0;
+  let words = text.trim().split(" ");
+  for (let word of words) {
+    if (word !== "") {
+      wordNumber++;
+    }
+  }
+  wordCount.textContent = String(wordNumber).padStart(2, "0");
+
+  // Sentence Count
+  let sentences = text.split(/[.!?]/);
+  let sentenceNumber = 0;
+  for (let sentence of sentences) {
+    if (sentence.trim() !== "") {
+      sentenceNumber++;
+    }
+  }
+  sentenceCount.textContent = String(sentenceNumber).padStart(2, "0");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// textarea.addEventListener("input", () => {
+//   const text = textarea.value;
+//   const characters = text.length;
+//   const words = text.trim().split(/\s+/).filter(word => word.length > 0).length;
+//   const sentences = text.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0).length;
+
+//   totalCharacters.textContent = characters;
+//   wordCount.textContent = words;
+//   sentenceCount.textContent = sentences;
+// });
